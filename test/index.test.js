2a0563bea05a7a538f0bc75cbd4552fbf3b5c774
Heyveryouaire <mathieu.pavy.29@gmail.com>
2021-11-04 14:43:39 +0100

    change value test

--- test/index.test.js
+++ test/index.test.js
@@ -4,6 +4,6 @@ const calc = require("../index")
 
 describe("Test add", () => {
     it("Should return 10", () => {
-        assert.equal(calc.add(3,6), 10)
+        assert.equal(calc.add(4,6), 10)
     })
 })
