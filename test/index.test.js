86e8bb7b47e9a09502e9f2d5052ba9a2276023b0
Heyveryouaire <mathieu.pavy.29@gmail.com>
2021-11-04 14:45:32 +0100

    error test

--- test/index.test.js
+++ test/index.test.js
@@ -4,6 +4,6 @@ const calc = require("../index")
 
 describe("Test add", () => {
     it("Should return 10", () => {
-        assert.equal(calc.add(4,6), 10)
+        assert.equal(calc.add(4,17), 10)
     })
 })
